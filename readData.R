de026686a23159470f605022c37b115ae2a966ea
baumfalk <jtbaumfalk@gmail.com>
2017-06-01 12:05:27 +0200

    added coupling to scripts

--- readData.R
+++ readData.R
@@ -11,6 +11,8 @@ movielensRatingDataRaw <- read.csv2("data/movielens/ratings.csv")
 movielensUserDataRaw <- read.csv2("data/movielens/users.csv")
 imdbDataRaw <- read.csv("data/imdb/imdbtop5000.csv")
 
+springfieldDataRaw <- read.csv2("data/springfield/urls_titles_springfield.txt")
+
 #remove trailing spaces in imdb titles
 trim.trailing <- function (x) sub("\\s+$", "", x)
 imdbData <- imdbDataRaw %>%
@@ -37,9 +39,9 @@ filmData <- movielensFilmDataRaw %>%
   fixYear() %>%
   inner_join(imdbData,by=c("Title"="movie_title_char", "Year"="title_year")) %>%
   fixGenres() %>%
-  unique()
+  unique() %>%
+  mutate(titlesSmall=tolower(iconv(Title,"WINDOWS-1252","UTF-8")))
   
-
 filmDataPivot <- filmData %>%
   mutate(exists=1) %>%
   spread(key=Genre,value=exists,fill=0)
@@ -50,4 +52,23 @@ movielensRatingDataRaw %>%
   arrange(desc(count))
 
 movielensRatingData = movielensRatingDataRaw[movielensRatingDataRaw$MovieID %in% filmDataPivot$MovieID,]
+movielensUserData = movielensUserDataRaw[movielensUserDataRaw$UserID %in% movielensRatingData$UserID,]
+a
+filmDataPivot <- filmDataPivot %>%
+  mutate(TitleAndYear = paste(titlesSmall,paste("(",Year,")",sep="")))
+
+springfieldDataRaw <- springfieldDataRaw %>%
+  mutate(titles=iconv(titles,"WINDOWS-1252","UTF-8"))%>%
+  mutate(titlesSmall=tolower(stringr::str_trim(titles)))
+
+springfieldData <- springfieldDataRaw[springfieldDataRaw$titlesSmall %in% filmDataPivot$TitleAndYear,]
+filmDataNotSpringfield <- filmDataPivot[!(filmDataPivot$TitleAndYear %in% springfieldDataRaw$titlesSmall),]
+
+##IMDB met sprinfield
+imdbData <- imdbData %>%
+  mutate(titlesSmall=tolower(iconv(movie_title_char,"WINDOWS-1252","UTF-8"))) %>%
+  mutate(TitleAndYear = paste(titlesSmall,paste("(",title_year,")",sep="")))
+
+springfieldDataIMDB <- springfieldDataRaw[springfieldDataRaw$titlesSmall %in% imdbData$TitleAndYear,]
+filmDataNotSpringfieldIMDB <- imdbData[!(imdbData$TitleAndYear %in% springfieldDataRaw$titlesSmall),]
 

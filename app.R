d1b6a835cd0bdc4b5b351439bcfbf34685185e60
atv22 <a@a.nl>
2017-06-16 15:32:33 +0200

    Youtube
    Packeges staan nog niet goed voor runnen online

--- app.R
+++ app.R
@@ -3,6 +3,11 @@ library(dplyr)
 library(ggplot2)
 library(wordcloud)
 library(tidytext)
+library(xml2)
+library(rvest)
+require(selectr)
+
+
 #library(recommenderlab)
 source("readData.R")
 source("uiApp.R")
@@ -11,4 +16,3 @@ source("serverApp.R")
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
